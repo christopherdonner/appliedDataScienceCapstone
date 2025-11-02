a8c0c3d7a40f00ae3efc5b27b6f2cc2cd877a5a3
Christopher Donner <cdonner@opentext.com>
2025-11-02 12:22:31 -0500

    adding remaining jupyter notebooks

---
+++ launchLocation.py
@@ -0,0 +1,192 @@
+import folium
+import pandas as pd
+
+# Import folium MarkerCluster plugin
+from folium.plugins import MarkerCluster
+# Import folium MousePosition plugin
+from folium.plugins import MousePosition
+# Import folium DivIcon plugin
+from folium.features import DivIcon
+
+# Download and read the `spacex_launch_geo.csv`
+# from js import fetch
+# import io
+
+# URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_geo.csv'
+# resp = await fetch(URL)
+# spacex_csv_file = io.BytesIO((await resp.arrayBuffer()).to_py())
+spacex_df=pd.read_csv('./spacex_launch_geo.csv')
+print(spacex_df)
+
+# Select relevant sub-columns: `Launch Site`, `Lat(Latitude)`, `Long(Longitude)`, `class`
+spacex_df = spacex_df[['Launch Site', 'Lat', 'Long', 'class']]
+launch_sites_df = spacex_df.groupby(['Launch Site'], as_index=False).first()
+launch_sites_df = launch_sites_df[['Launch Site', 'Lat', 'Long']]
+print(launch_sites_df)
+
+# Select relevant sub-columns: `Launch Site`, `Lat(Latitude)`, `Long(Longitude)`, `class`
+spacex_df = spacex_df[['Launch Site', 'Lat', 'Long', 'class']]
+launch_sites_df = spacex_df.groupby(['Launch Site'], as_index=False).first()
+launch_sites_df = launch_sites_df[['Launch Site', 'Lat', 'Long']]
+print(launch_sites_df)
+
+# Start location is NASA Johnson Space Center
+nasa_coordinate = [29.559684888503615, -95.0830971930759]
+site_map = folium.Map(location=nasa_coordinate, zoom_start=10)
+
+# Create a blue circle at NASA Johnson Space Center's coordinate with a popup label showing its name
+circle = folium.Circle(nasa_coordinate, radius=1000, color='#d35400', fill=True).add_child(folium.Popup('NASA Johnson Space Center'))
+# Create a blue circle at NASA Johnson Space Center's coordinate with a icon showing its name
+marker = folium.map.Marker(
+    nasa_coordinate,
+    # Create an icon as a text label
+    icon=DivIcon(
+        icon_size=(20,20),
+        icon_anchor=(0,0),
+        html='<div style="font-size: 12; color:#d35400;"><b>%s</b></div>' % 'NASA JSC',
+        )
+    )
+site_map.add_child(circle)
+site_map.add_child(marker)
+
+# Initialize the map
+site_map = folium.Map(location=nasa_coordinate, zoom_start=4)
+# For each launch site, add a Circle object based on its coordinate (Lat, Long) values. In addition, add Launch site name as a popup label
+for index, row in launch_sites_df.iterrows():
+    coordinate = [row['Lat'], row['Long']]
+    folium.Circle(coordinate, radius=1000, color='#000000', fill=True).add_child(folium.Popup(row['Launch Site'])).add_to(site_map)
+    folium.map.Marker(coordinate, icon=DivIcon(icon_size=(20,20),icon_anchor=(0,0), html='<div style="font-size: 12; color:#d35400;"><b>%s</b></div>' % row['Launch Site'], )).add_to(site_map)
+site_map
+
+spacex_df.tail(10)
+
+
+# Apply a function to check the value of `class` column
+# If class=1, marker_color value will be green
+# If class=0, marker_color value will be red
+# Function to assign color to launch outcome
+def assign_marker_color(launch_outcome):
+    if launch_outcome == 1:
+        return 'green'
+    else:
+        return 'red'
+    
+spacex_df['marker_color'] = spacex_df['class'].apply(assign_marker_color)
+spacex_df.tail(10)
+
+# Add marker_cluster to current site_map
+# site_map.add_child(marker_cluster)
+
+# # for each row in spacex_df data frame
+# # create a Marker object with its coordinate
+# # and customize the Marker's icon property to indicate if this launch was successed or failed, 
+# # e.g., icon=folium.Icon(color='white', icon_color=row['marker_color']
+# for index, record in spacex_df.iterrows():
+#     # TODO: Create and add a Marker cluster to the site map
+#     # marker = folium.Marker(...)
+#     marker_cluster.add_child(marker)
+
+site_map
+
+# Add Mouse Position to get the coordinate (Lat, Long) for a mouse over on the map
+formatter = "function(num) {return L.Util.formatNum(num, 5);};"
+mouse_position = MousePosition(
+    position='topright',
+    separator=' Long: ',
+    empty_string='NaN',
+    lng_first=False,
+    num_digits=20,
+    prefix='Lat:',
+    lat_formatter=formatter,
+    lng_formatter=formatter,
+)
+
+site_map.add_child(mouse_position)
+site_map
+
+from math import sin, cos, sqrt, atan2, radians
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
+# find coordinate of the closet coastline
+# e.g.,: Lat: 28.56367  Lon: -80.57163
+# distance_coastline = calculate_distance(launch_site_lat, launch_site_lon, coastline_lat, coastline_lon)
+
+# Create a `folium.PolyLine` object using the coastline coordinates and launch site coordinate
+# lines=folium.PolyLine(locations=coordinates, weight=1)
+# site_map.add_child(lines)
+
+# # Create a marker with distance to a closest city, railway, highway, etc.
+# # Draw a line between the marker to the launch site
+# closest_highway = 28.56335, -80.57085
+# closest_railroad = 28.57206, -80.58525
+# closest_city = 28.10473, -80.64531
+
+# distance_highway = calculate_distance(launch_site_lat, launch_site_lon, closest_highway[0], closest_highway[1])
+# print('distance_highway =',distance_highway, ' km')
+# distance_railroad = calculate_distance(launch_site_lat, launch_site_lon, closest_railroad[0], closest_railroad[1])
+# print('distance_railroad =',distance_railroad, ' km')
+# distance_city = calculate_distance(launch_site_lat, launch_site_lon, closest_city[0], closest_city[1])
+# print('distance_city =',distance_city, ' km')
+
+# # closest highway marker
+# distance_marker = folium.Marker(
+#    closest_highway,
+#    icon=DivIcon(
+#        icon_size=(20,20),
+#        icon_anchor=(0,0),
+#        html='<div style="font-size: 12; color:#d35400;"><b>%s</b></div>' % "{:10.2f} KM".format(distance_highway),
+#        )
+#    )
+# site_map.add_child(distance_marker)
+# # closest highway line
+# coordinates = [[launch_site_lat,launch_site_lon],closest_highway]
+# lines=folium.PolyLine(locations=coordinates, weight=1)
+# site_map.add_child(lines)
+
+# # closest railroad marker
+# distance_marker = folium.Marker(
+#    closest_railroad,
+#    icon=DivIcon(
+#        icon_size=(20,20),
+#        icon_anchor=(0,0),
+#        html='<div style="font-size: 12; color:#d35400;"><b>%s</b></div>' % "{:10.2f} KM".format(distance_railroad),
+#        )
+#    )
+# site_map.add_child(distance_marker)
+# # closest railroad line
+# coordinates = [[launch_site_lat,launch_site_lon],closest_railroad]
+# lines=folium.PolyLine(locations=coordinates, weight=1)
+# site_map.add_child(lines)
+
+# # closest city marker
+# distance_marker = folium.Marker(
+#    closest_city,
+#    icon=DivIcon(
+#        icon_size=(20,20),
+#        icon_anchor=(0,0),
+#        html='<div style="font-size: 12; color:#d35400;"><b>%s</b></div>' % "{:10.2f} KM".format(distance_city),
+#        )
+#    )
+# site_map.add_child(distance_marker)
+# # closest city line
+# coordinates = [[launch_site_lat,launch_site_lon],closest_city]
+# lines=folium.PolyLine(locations=coordinates, weight=1)
+# site_map.add_child(lines)
+
